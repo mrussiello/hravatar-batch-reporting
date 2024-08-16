36165086ba17e248dccfc9cb88d1c1632a461572
miker <miker@192.168.1.93>
2024-08-16 14:45:43 -0400

    2024CR932	8/15/2024	MikeR	Feature	Modify CTB to allow for download of PDF or Word or other files from within an item.

--- src/main/java/com/tm2batch/file/UploadedUserFileType.java
+++ src/main/java/com/tm2batch/file/UploadedUserFileType.java
@@ -4,9 +4,16 @@ package com.tm2batch.file;
 public enum UploadedUserFileType
 {
     RESPONSE(0,"User Response"),
-    REMOTE_PROCTORING(100,"Remote Proctoring"),
-    REMOTE_PROCTORING_IMAGES(101,"Remote Proctoring Images"),
-    REMOTE_PROCTORING_ID(102,"Remote Proctoring ID Capture");
+    REMOTE_PROCTORING(100,"Remote Proctoring Audio/Video (with Image Thumbs)"),
+    REMOTE_PROCTORING_IMAGES(101,"Remote Proctoring Image Thumbs (Only)"),
+    REMOTE_PROCTORING_ID(102,"Remote Proctoring IDs (Only)"),
+    REF_CHECK_IMAGES(201,"Reference Check Candidate Image Thumbs (Only)"),
+    REF_CHECK_ID(202,"Reference Check Candidate IDs (Only)"),
+    REF_CHECK_RATER(211,"Reference Check Rater Images (Only)"),
+    REF_CHECK_RATER_ID(212,"Reference Check Rater IDs (Only)"),
+    REF_CHECK_RATER_COMMENT(220,"Reference Check Rater Comment" ),
+    CT5(230,"CT5 media" ),
+    CT5_GENERAL(231,"CT5 General Uploaded File (for download inside item)" );
 
 
 
